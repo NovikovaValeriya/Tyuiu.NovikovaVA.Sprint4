38550ad8d3aef54f73c4ff08dfdf696e432a41b8
NovikovaValeriya <valeriya_novikova_06@mail.ru>
2024-11-06 11:22:51 +0500

    Task3

--- Tyuiu.NovikovaVA.Sprint4.Task3.V15/Program.cs
+++ Tyuiu.NovikovaVA.Sprint4.Task3.V15/Program.cs
@@ -1,48 +1,52 @@
-﻿using System.Security.Cryptography;
-using Tyuiu.NovikovaVA.Sprint4.Task2.V5.Lib;
-namespace Tyuiu.NovikovaVA.Sprint4.Task2.V5
+﻿using Tyuiu.NovikovaVA.Sprint4.Task3.V15.Lib;
+namespace Tyuiu.NovikovaVA.Sprint4.Task3.V15
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
+            int[,] array = new int[5, 5] { { 7, 4, 2, 5, 3 },
+                                          { 4, 3, 2, 5, 6 },
+                                          { 6, 3, 4, 7, 5 },
+                                          { 5, 7, 4, 3, 8 },
+                                          { 7, 8, 8, 5, 6 } };
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
             DataService ds = new DataService();
             Console.Title = "Спринт #4 | Выполнила: Новикова В.А. | ИСТН-б-24-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
             Console.WriteLine("* Тема:                                                                   *");
-            Console.WriteLine("* Задание #2                                                              *");
-            Console.WriteLine("* Вариант #24                                                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #15                                                             *");
             Console.WriteLine("* Выполнила: Новикова Валерия Александрован | ИСТН-б-24-1                 *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дан одномерный целочисленный массив на 14 элементов заполненный         *");
-            Console.WriteLine("* случайными в диапазоне от 3 до 9 подсчитать произведение                *");
-            Console.WriteLine("* четных элементов массива.                                               *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+            Console.WriteLine("* статическими значениями в диапазоне от 2 до 9.                          *");
+            Console.WriteLine("* Подсчитайте количество нечетных элементов во всем массиве.              *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.Write("Введите количество элементов массива: ");
-            int len = Convert.ToInt32(Console.ReadLine());
-            int[] array = new int[len];
-            for (int i = 0; i <= len - 1; i++)
+            Console.Write("Массив: ");
+            Console.WriteLine();
+            for (int i = 0; i < rows; i++)
             {
-                array[i] = rnd.Next(3, 9);
-            }
-            Console.WriteLine("Массив:");
-            for (int i = 0;i <= len - 1; i++)
-            {
-                Console.WriteLine(array[i]+"\t");
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{array[i, j]}\t");
+                }
+                Console.WriteLine();
             }
             Console.WriteLine();
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("произведение всех чётных = " + ds.Calculate(array));
+            Console.WriteLine("Количество нечётных элементов = " + ds.Calculate(array));
             Console.ReadKey();
+
         }
     }
 }
